a6096c83bff88b87925cce2cc35c3e3f14f4b0b1
bferrarini <johndoe@example.com>
2023-12-20 23:19:50 +0100

    'loops' command added to main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import argparse
 from utility import GpsExtractor, Unspooler, prune_dataset
 from blurLib import AverageSytheticBlur
 import os
-
+import shutil
+import re
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -47,6 +48,16 @@ def parse_args():
     all_parser.add_argument("-s", "--sfps", dest="source_fps", type=int, required=False, default=240 ,help="Source FPS. The deafult is 240.")
     all_parser.add_argument("-r", "--fps", dest="fps", type=int, required=True ,help="Target FPS.")
     all_parser.add_argument("-O", "--override", dest="override", action="store_true" ,help="Override the output filder's content.")
+
+    # loops
+    loop_parser = subparsers.add_parser("loops")
+    loop_parser.add_argument("-R", "--reference-dataset", dest="img_reference_folder", help="Folder where the images to be used as a reference are stored (i.e. 1 FPS dataset)")
+    loop_parser.add_argument("-o", "--out-folder", dest="output", type=str, required=True, help="Destination Folder for the loops.")
+    loop_parser.add_argument("-B", "--blurred-datasets", dest="root_blurred_folder", help="Folder where the blurred versions of a datasets are stored.")
+    loop_parser.add_argument("-b", "--blur-intesities", dest="nFrames", type=int, nargs='+', required=True, help="Used to identify the folder where the lurred images are stored (i.e. the product avg-blur)")
+    loop_parser.add_argument("--blur-prefix", dest="bPrefix", type=str, default="AVGBLUR_B-", required=False, help="Fix the problem of potentially different namning convention of the blurred directories.")
+    loop_parser.add_argument("--ref_gps", dest="ref_gps_file", type=str, required=True, help="GPS file for the reference traversal.")
+    loop_parser.add_argument("--query_gps", dest="query_gps_file", type=str, required=True, help="GPS file for the query traversal.")
 
     return parser.parse_args()
 
@@ -128,11 +139,77 @@ def command_prune_dataset(args):
                   offset=args.offset, tail=args.tail, 
                   override=args.override, file_filter= ".*\.(jpg|png)")
 
+# TEST
+# loops -R D:\\datasets\\MotionBlur\\LUZZARA-03B_1FPS -B D:\\datasets\\MotionBlur\\LUZZARA-03A_BLURRED -o D:\\datasets\\MotionBlur\\LUZZARA-03AB --ref_gps=D:\\datasets\\MotionBlur\\LUZZARA-03A.csv --query_gps=D:\\datasets\\MotionBlur\\LUZZARA-03B.csv -b 80 120 240
+def command_build_traversals(args):
+    '''
+    Copeis the imagtes and file to the reuired structure to run the experiments.
+    The GT must be computed later, loop by loop, using the dedicated tool provided in VPR_evelaution_framework
+    '''
+    # reference data dir. Generally, it is a 'low' frame rate dataset obtained with the 'prune' command
+    rDir = args.img_reference_folder
+    # blurred directory
+    bDir = args.root_blurred_folder
+    # blur levels
+    outDir = args.output
+    # reference GPS
+    refGPS = args.ref_gps_file
+    # query GPS
+    queryGPS = args.query_gps_file
+    # blur Levels
+    bLevels = args.nFrames
+    # blur prefix
+    bprefix = args.bPrefix
+
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+
+        
+    for bl in bLevels:
+        blSource = os.path.join(bDir, "{:s}{:03d}".format(bprefix, int(bl)))
+        if os.path.exists(blSource) and os.path.isdir(blSource):
+            loopDir = os.path.join(outDir, "{:03d}".format(int(bl)))
+            if not os.path.exists(loopDir):
+                os.makedirs(loopDir)
+            target_refGPS = os.path.join(loopDir, "referenceGPS.csv")
+            target_queryGPS = os.path.join(loopDir, "queryGPS.csv")
+            shutil.copyfile(refGPS, target_refGPS)
+            shutil.copyfile(queryGPS, target_queryGPS)
+
+            target_reference = os.path.join(loopDir, "reference")
+            if not os.path.exists(target_reference):
+                os.makedirs(target_reference)
+            source_files = [os.path.join(rDir, f) for f in os.listdir(rDir) if re.search(".*\.(jpg|png)",f)]
+            destination_files = [os.path.join(target_reference, f) for f in os.listdir(rDir) if re.search(".*\.(jpg|png)",f)]
+            c = 0
+            for s,d in zip(source_files,destination_files):
+                shutil.copy(src = s, dst= d)
+                c += 1
+            print(f"{c} files copied from {rDir} to {target_reference}.")
+
+            target_query = os.path.join(loopDir, "query")
+            if not os.path.exists(target_query):
+                os.makedirs(target_query)
+
+            source_files = [os.path.join(blSource, f) for f in os.listdir(blSource) if re.search(".*\.(jpg|png)",f)]
+            destination_files = [os.path.join(target_query, f) for f in os.listdir(blSource) if re.search(".*\.(jpg|png)",f)]
+            c = 0
+            for s,d in zip(source_files,destination_files):
+                shutil.copy(src = s, dst= d)
+                c += 1
+            print(f"{c} files copied from {blSource} to {target_query}.")
+
+        else:
+            print(f"BL {bl} skipped. {blSource} not found.")
+        
+
+
 COMMANDS = {
     "gps": command_gps_extract,
     "extract": command_frame_extract,
     "avg-blur": command_blur_dataset,
     "prune": command_prune_dataset,
+    "loops" : command_build_traversals,
 }
 
 
--- test_plot.py
+++ test_plot.py
@@ -4,6 +4,11 @@ from utility import GpsExtractor, Visualizer
 
 fn1 = "D:\\datasets\\MotionBlur\\for_testing\\LUZZARA-03A.MP4"
 fn2 = "D:\\datasets\\MotionBlur\\for_testing\\LUZZARA-03B.MP4"
+fn3 = "D:\\datasets\\MotionBlur\\LUZZARA-04\\loop1.MP4"
+fn4 = "D:\\datasets\\MotionBlur\\LUZZARA-04\\loop2.MP4"
+fn5 = "D:\\datasets\\MotionBlur\\LUZZARA-04\\loop3.MP4"
+fn6 = "D:\\datasets\\MotionBlur\\LUZZARA-04\\loop4.MP4"
+fn7 = "D:\\datasets\\MotionBlur\\LUZZARA-04\\loop5.MP4"
 gps_map = "D:\\datasets\\MotionBlur\\for_testing\\LUZZARA-03.html" 
 
 visualizer = Visualizer()
@@ -13,6 +18,16 @@ visualizer = Visualizer()
 
 gps1 = GpsExtractor(fn1, fill_the_gaps=False)
 gps2 = GpsExtractor(fn2, fill_the_gaps=False)
-visualizer.append("AAA",gps1)
-visualizer.append("BBB", gps2)
+gps3 = GpsExtractor(fn3, fill_the_gaps=False)
+gps4 = GpsExtractor(fn4, fill_the_gaps=False)
+gps5 = GpsExtractor(fn5, fill_the_gaps=False)
+gps6 = GpsExtractor(fn6, fill_the_gaps=False)
+gps7 = GpsExtractor(fn7, fill_the_gaps=False)
+#visualizer.append("A1",gps1)
+#visualizer.append("A2", gps2)
+visualizer.append("B1", gps3)
+visualizer.append("B2", gps4)
+visualizer.append("B3", gps5)
+#visualizer.append("B4", gps5)
+#visualizer.append("B5", gps7)
 visualizer.draw_map(gps_map)
