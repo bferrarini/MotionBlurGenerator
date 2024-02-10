db8fa4e0459ec78904bd86a2b11e64265b9d21fb
bferrarini <bferrarini@example.com>
2024-02-10 15:50:20 +0100

    fff

--- scripts/generate_blur_ammend.bat
+++ scripts/generate_blur_ammend.bat
@@ -47,17 +47,17 @@ rem%py% %main% gps -v %1\raw_videos\%3 -o %1\gps\%2.txt --fill-the-gaps
 REM STEP 1: unroll
 rem%py% %main% extract -v %1\raw_videos\%3 -o %1\unrolled\%2 -m %1\mute_videos\MUTE_%3 -W 960 -H 540 -f jpg
 
-REMSTEP 2: BLURRING
+REM STEP 2: BLURRING
 rem(for %%a in (%bls%) do ( 
 rem  %py% %main% avg-blur -d %1\unrolled\%2 -o %1\blurred\%2 -b %%a
 rem))
 
 REM STEP 3: PRUNING TO 1 FPS
 
-(for %%a in (%bls%) do ( 
-    %py% %main% prune -d %1\blurred\%2/AVGBLUR_B-%%a -o %1\pruned_blurred\%2\%%a --fps=%%a --sfps=240 --override 
-    echo "BL %%a PROCESSED!"
-))
+rem(for %%a in (%bls%) do ( 
+rem    %py% %main% prune -d %1\blurred\%2/AVGBLUR_B-%%a -o %1\pruned_blurred\%2\%%a --fps=%%a --sfps=240 --override 
+rem    echo "BL %%a PROCESSED!"
+rem))
 
 REM STEP 4: GENRATING 1_FPS references
-rem%py% %main% prune -d %1\unrolled\%2 -o %1\low_fps\%2 --fps=1 --sfps=240 --override
+%py% %main% prune -d %1\unrolled\%2 -o %1\low_fps\%2 --fps=1 --sfps=240 --override
