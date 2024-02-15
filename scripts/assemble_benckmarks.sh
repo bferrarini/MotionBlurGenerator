21c29401a8afee25c91d01f184e359d9bb52fe20
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-15 10:27:06 +0100

    luzzara 11

--- scripts/assemble_benckmarks.sh
+++ scripts/assemble_benckmarks.sh
@@ -3,9 +3,9 @@
 #source /home/main/vscode_workplace/MotionBlurGenerator/.venv/bin/activate
 py=python3
 main=/home/main/vscode_workplace/MotionBlurGenerator/main.py
-#dataset_name=LUZZARA-04
+dataset_name=LUZZARA-04
 #dataset_name=GUASTALLA-03
-dataset_name=CASONI-01
+#dataset_name=CASONI-01
 prefix=${dataset_name}
 root_dir=/home/main/Documents/MotionBlur/${dataset_name}
 
@@ -13,7 +13,8 @@ root_dir=/home/main/Documents/MotionBlur/${dataset_name}
 #target_pairs=("02:01" "03:01" "04:01" "05:01" "06:01" "04:02")
 #target_pairs=("04:04")
 #target_pairs=("03:09" "09:05" "05:09" "09:03" "09:09")
-target_pairs=("09:07" "11:07" "01:09" "01:11" "07:09" "07:11")
+#target_pairs=("09:07" "11:07" "01:09" "01:11" "07:09" "07:11")
+target_pairs=("13:01" "13:13" "13:04" "01:13" "11:13" "04:11")
 
 for pair in ${target_pairs[@]}
 do 
